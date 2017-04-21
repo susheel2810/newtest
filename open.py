2ab66776ead9d243e9324f391c02f581a33d304d
LENOVO <LENOVO@m>
2017-04-21 17:39:20 +0530

    new script

---
+++ open.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 21, 2017
+
+@author: LENOVO
+'''
+
+if __name__ == '__main__':
+    file=open("C:\\Users\\LENOVO\\sushi.txt","r")
+    print(file.read())
+    print(file.tell())
+    for line in file:
+        print(line)
+        
+    print("File Name:" +file.name)
+    print("Mode:"+file.mode)
+    print("File Closed:"+str(file.closed))
+    
